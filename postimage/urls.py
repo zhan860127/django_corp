7fdcf9b0b5f64e4e858fe59f298471bd402d707e
tony zhan <zhan860127@gmail.com>
2021-07-15 16:25:34 +0800

    add_to_heroku

---
+++ postimage/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from postimage import views as homeViews
+from django.conf.urls import url
+from django.conf.urls import url
+from django.contrib import admin
+from super_resolution import views as homeViews
+
+from django.urls import path,re_path
+
+
+from . import views
+urlpatterns = [
+    path('', views.index, name='Index'),
+    url('upload_avatar/',homeViews.upload_avatar), # 上傳頭像
+    re_path(r'download/(?P<file_path>.*)/$', views.file_response_download1, name='file_download'),
+
+]
+
+
